8e72a35e1da2600bd5b3e9a0c1433fad3841a19b
bj299 <122151794+bj299@users.noreply.github.com>
2023-04-30 16:57:24 +0900

    DA_Junior_교육세션_HTML 과제제출

---
+++ script.js
@@ -0,0 +1,13 @@
+const a = "front end";
+
+function printConsole() {
+  console.log(a);
+}
+
+let b = "written by Byung Joo";
+
+function printWriter() {
+  b = "wrtten by Byung Joo Jeong";
+  const repeat = prompt(b);
+  alert(repeat);
+}
